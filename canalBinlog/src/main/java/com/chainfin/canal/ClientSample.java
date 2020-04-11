21a91c2f8be93ce4e111af3652020d8555a617aa
cy2821189599 <2821189599@qq.com>
2020-04-11 10:05:12 +0800

    MySQL binlog 解析工具canal使用，canal客户端读取

--- canalBinlog/src/main/java/com/chainfin/canal/ClientSample.java
+++ canalBinlog/src/main/java/com/chainfin/canal/ClientSample.java
@@ -39,7 +39,6 @@ public class ClientSample {
                     try {
                         Thread.sleep( 1000 );
                     } catch (InterruptedException e) {
-                        System.out.println( e.getStackTrace() );
                     }
                 } else {
                     emptyCount = 0;
