35566c9112469c996b896ab53e49acfad4e68d1d
kenor <kenor.chen@qq.com>
2020-08-09 15:34:18 +0800

    read from hdfs or write to hdfs

--- flinkSymtax/src/main/scala/flink/dataSetApi/WordCount.scala
+++ flinkSymtax/src/main/scala/flink/dataSetApi/WordCount.scala
@@ -6,13 +6,15 @@ import org.apache.flink.core.fs.FileSystem
 object WordCount {
   def main(args: Array[String]): Unit = {
     val env = ExecutionEnvironment.getExecutionEnvironment
-    val filePath = "E:\\ReciveFile\\MiXing\\day18\\dataSource\\subjectaccess\\access.txt"
-    val inputDateSet: DataSet[String] = env.readTextFile(filePath)
+    val filePath = "E:\\access.txt"
+    //    val inputDateSet: DataSet[String] = env.readTextFile(filePath)//read from  local fileSystem
+    val inputDateSet: DataSet[String] = env.readTextFile("hdfs://ns1/data/flinkLearn/wordCount") //read from hdfs fileSystem
     import org.apache.flink.api.scala._
     val count = inputDateSet.flatMap(_.split("\\s")).map((_, 1)).groupBy(0).sum(1)
-//    count.print()
-    count.writeAsText("hdfs://ns1/data/flinkLearn/wordCount",FileSystem.WriteMode.OVERWRITE)
-    env.execute(this.getClass.getSimpleName)
+    count.print()
+    //    count.writeAsText("hdfs://ns1/data/flinkLearn/wordCount",FileSystem.WriteMode.OVERWRITE)//write to hdfs fileSystem
+    // if there is "sink", there will call env.execute() function
+    //    env.execute(this.getClass.getSimpleName)
   }
 
 }
