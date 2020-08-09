1def3164ae76a80e911fd9342d0eb28eb79ba690
kenor <kenor.chen@qq.com>
2020-08-09 15:59:37 +0800

    read from hdfs or write to hdfs

--- flinkSymtax/src/main/scala/flink/dataSetApi/WordCount.scala
+++ flinkSymtax/src/main/scala/flink/dataSetApi/WordCount.scala
@@ -6,15 +6,16 @@ import org.apache.flink.core.fs.FileSystem
 object WordCount {
   def main(args: Array[String]): Unit = {
     val env = ExecutionEnvironment.getExecutionEnvironment
-    val filePath = "E:\\access.txt"
-    //    val inputDateSet: DataSet[String] = env.readTextFile(filePath)//read from  local fileSystem
-    val inputDateSet: DataSet[String] = env.readTextFile("hdfs://ns1/data/flinkLearn/wordCount") //read from hdfs fileSystem
+    val filePath = "E:\\temp\\input\\2.txt"
+    val inputDateSet: DataSet[String] = env.readTextFile(filePath) //read from  local fileSystem
+    //    val inputDateSet: DataSet[String] = env.readTextFile("hdfs://ns1/data/flinkLearn/wordCount") //read from hdfs fileSystem
     import org.apache.flink.api.scala._
     val count = inputDateSet.flatMap(_.split("\\s")).map((_, 1)).groupBy(0).sum(1)
-    count.print()
-    //    count.writeAsText("hdfs://ns1/data/flinkLearn/wordCount",FileSystem.WriteMode.OVERWRITE)//write to hdfs fileSystem
+    //    count.print()
+    count.writeAsText("hdfs://ns1/data/flinkLearn/wordCount", FileSystem.WriteMode.OVERWRITE)
+    .setParallelism(1)//write to hdfs fileSystem,the default Parallelism is the number of cores
     // if there is "sink", there will call env.execute() function
-    //    env.execute(this.getClass.getSimpleName)
+    env.execute(this.getClass.getSimpleName)
   }
 
 }
